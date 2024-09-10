0aec1e12660f24ef0876ad20b5bbf8cb85db4466
erik-dunteman <erikdunteman@gmail.com>
2024-09-09 18:09:37 -0700

    wip

--- mmlu_eval.py
+++ mmlu_eval.py
@@ -77,9 +77,9 @@ subject2category = {
 
 class MMLUEval(Eval):
     def __init__(self, num_examples: int | None = None):
-        df = pandas.read_csv(
-            bf.BlobFile("https://openaipublic.blob.core.windows.net/simple-evals/mmlu.csv")
-        )
+        # wget https://openaipublic.blob.core.windows.net/simple-evals/mmlu.csv
+        df = pandas.read_csv("mmlu.csv")
+
         examples = [row.to_dict() for _, row in df.iterrows()]
         if num_examples:
             examples = random.Random(0).sample(examples, num_examples)
