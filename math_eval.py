326580f2c27c908ffdf24f462a9d8151d710522e
erik-dunteman <erikdunteman@gmail.com>
2024-09-09 18:20:44 -0700

    use math_eval

--- math_eval.py
+++ math_eval.py
@@ -6,6 +6,7 @@ https://arxiv.org/abs/2103.03874
 
 import random
 import re
+import os
 
 import blobfile as bf
 import pandas
@@ -25,9 +26,9 @@ Remember to put your answer on its own line after "Answer:", and you do not need
 
 class MathEval(Eval):
     def __init__(self, equality_checker: SamplerBase, num_examples: int | None = None):
-        df = pandas.read_csv(
-            bf.BlobFile("https://openaipublic.blob.core.windows.net/simple-evals/math_test.csv")
-        )
+        # wget https://openaipublic.blob.core.windows.net/simple-evals/math_test.csv
+        path = os.path.dirname(os.path.abspath(__file__))
+        df = pandas.read_csv(os.path.join(path, "math_test.csv"))
         examples = [row.to_dict() for _, row in df.iterrows()]
         if num_examples:
             examples = random.Random(0).sample(examples, num_examples)
